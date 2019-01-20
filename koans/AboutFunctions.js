d212bbef00bc46af81802d9b0dfba24282af048a
Marissa <marissa.shieh@gmail.com>
2019-01-20 16:35:45 -0500

    Rest of function tests

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -106,6 +106,9 @@ describe("About Functions", function() {
       // An internal comment
       return a * b;
     };
-    expect(multiply.toString()).toBe(FILL_ME_IN);
+    expect(multiply.toString()).toBe("function(a, b) {
+      // An internal comment
+      return a * b;
+    }");
   });    
 });
